27783021983d9245956bb981e3226531af7c0d71
jonaygarciav <jonaygarciav@gmail.com>
2019-10-17 15:21:05 +0100

    Se añade la lógica de la aplicación

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lv_paises;
+
+    private String[] paises = new String[] {"Portugal", "España", "Francia", "Alemania", "Italia", "Suiza" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lv_paises = findViewById(R.id.lv_paises);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, paises);
+
+        lv_paises.setAdapter(adapter);
+
+        lv_paises.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), paises[position], Toast.LENGTH_SHORT).show();
+            }
+
+        });
     }
 }
